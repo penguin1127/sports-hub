276d0347ee3141c631c9d52c645be9e87f896432
Merongcity12 <bum11174@gmail.com>
2025-05-30 01:34:06 +0900

    Delete frontend directory

--- frontend/src/mock/mockUsers.ts
+++
@@ -1,8 +0,0 @@
-// ✅ mock/mockUsers.ts
-export const mockUsers = [
-    { id: "u1", name: "감독님" },
-    { id: "u2", name: "선수1" },
-    { id: "u3", name: "팀장" },
-    { id: "u4", name: "마포선수" }
-  ]
-  
