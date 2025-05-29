3eedb2407c1a4c3834fcafd6d61d8ee7b1a2e33f
Merongcity12 <bum11174@gmail.com>
2025-05-30 01:33:58 +0900

    Delete backend directory

--- backend/src/main/java/com/example/backend/entity/RecruitApplicationHistory.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.entity;
-
-// 신청 내역을 N개월 동안 저장하는 기능을 추가하기 위해 만듦.
-public class RecruitApplicationHistory {
-}
--- backend/src/main/java/com/example/backend/entity/RecruitPostHistory.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.entity;
-
-// 팀 모집 공고를 N개월 동안 저장하는 기능을 추가하기 위해 만듦.
-public class RecruitPostHistory {
-}
