199b891159328131f37d77bf4d475d3b87ebd247
Merongcity12 <bum11174@gmail.com>
2025-06-14 19:08:50 +0900

    백엔드 용병 관련 테이블 정리 및 알림 서비스 거의 완성

--- backend/src/main/java/com/example/backend/Enum/ApplicationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.Enum;
-
-public enum ApplicationStatus {
-    PENDING, APPROVED, REJECTED // 보류, 수락, 거부
-}
--- backend/src/main/java/com/example/backend/repository/MercenaryApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.MercenaryApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 용병 신청 테이블 관련 CRUD를 수행함.
-public interface MercenaryApplicationRepository extends JpaRepository<MercenaryApplication, Long> {
-
-}
